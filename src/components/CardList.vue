933c770f97045720d563758bc4b0753d50c62216
Zheldybin Anatoly <zheldybin.aa@mail.ru>
2024-03-24 17:26:44 +0300

    add card and cardList

---
+++ src/components/CardList.vue
@@ -0,0 +1,25 @@
+<script setup>
+import Card from './Card.vue'
+const onClickAdd = () => {
+  alert('Добавить!')
+}
+</script>
+
+<template>
+  <div class="grid grid-cols-4 gap-5">
+    <Card
+      :price="19900"
+      title="Мужские Кроссовки Nike Blazer Mid Suede"
+      image-url="/sneakers/sneakers-1.jpg"
+      :onClickAdd="onClickAdd"
+    />
+    <Card />
+    <Card />
+    <Card />
+    <Card />
+    <Card />
+    <Card />
+    <Card />
+    <Card />
+  </div>
+</template>
